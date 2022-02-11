251f5597286349380a7c222deca9d851e600907e
irazramin <irazramin@gmail.com>
2022-02-11 11:44:46 +0600

    bank deposit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const loginBtn = document.getElementById('login-btn');
+
+
+
+
+document.getElementById('login-btn').addEventListener('click', function () {
+  const email = document.getElementById('login-email');
+  const password = document.getElementById('login-password');
+
+  const emailValue = email.value
+  const passwordValue = password.value
+
+  if (emailValue === 'irazramin@gmail.com' && passwordValue === '1234') {
+    window.location.href = 'bank.html';
+  }
+  email.value = '';
+  password.value = '';
+});
+
+
